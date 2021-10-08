2fe42ce47b1e886cae97b37b19f2a68946d90926
qasem-moh <2171246@s.aou.edu.jo>
2021-10-08 03:39:18 +0300

    portfolio

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './Sidebar.css';
+import { HashLink as Link } from 'react-router-hash-link';
+import {  FiExternalLink } from 'react-icons/fi';
+import logo from '../images/My-logo4.ico'
+
+class Sidebar extends Component {
+    render() {
+       
+        return (
+            <div className="sidebar">
+                <img src={logo} />
+                <h1><Link smooth to="/#start" className="h1_links">Qasem Mohammad</Link></h1>
+                <p style={{color:'black',fontWeight:'bold'}} className="gmail"> Qasemcoder2020@gmail.com </p>
+                <ul className="sidebar-nav">
+                    <li className="sidebar-nav-items"><Link smooth to="/#about" className="links">About</Link></li>
+                    <li className="sidebar-nav-items"><Link smooth to="/#education" className="links" >Education</Link></li>
+                    <li className="sidebar-nav-items"><Link smooth to="/#interest" className="links">Interest</Link></li>
+                    {/* <li className="sidebar-nav-items"><a href="http://www.bloggingpeek.com" target="_blank" rel="opener noreferrer" className="links"> Blog<FiExternalLink/></a></li> */}
+                </ul>
+    
+                <div className="flip-card-back">
+                    <ul className="sidebar-nav">
+                        <li className="sidebar-nav-icons"><a href="https://www.linkedin.com/in/qasem-mohammad-593459200/"rel="opener noreferrer" target="_blank" class="fa fas fa-linkedin fa-lg"></a></li>
+                        <li className="sidebar-nav-icons"> <a href="https://github.com/qasem-moh" rel="opener noreferrer" target="_blank" class="fa fas fa-github fa-lg"></a></li>
+                        <li className="sidebar-nav-icons"> <a href="https://twitter.com/coderqasemmoh" rel="opener noreferrer" target="_blank" className="fa fas fa-twitter fa-lg"></a></li>
+                        <li className="sidebar-nav-icons"> <a href="https://www.instagram.com/qasem_kmc/" rel="opener noreferrer" target="_blank" class="fa fas fa-instagram fa-lg"></a></li>
+                        <li className="sidebar-nav-icons"> <a href="Qasemcoder2020@gmail.com" rel="opener noreferrer" target="_blank" class="fa fas fa-envelope fa-lg"></a></li>
+                        <li className="sidebar-nav-icons"> <a href="https://web.facebook.com/qaseem.mohamed.796/" rel="opener noreferrer" target="_blank" class="fa fas fa-facebook fa-lg"></a></li>
+                    </ul>
+                </div>
+              
+            </div>
+        )
+    }
+}
+
+export default Sidebar
